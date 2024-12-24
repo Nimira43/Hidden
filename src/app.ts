a149a033eb8ed1c99adcf7ea51b7a8b9cf6e6cf7
Nimira43 <n.rabone72@aol.com>
2024-12-24 00:54:07 +0000

    Set camera attach control to true. Wrote scripts to build and run project in package.json

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,9 @@ class Game {
 
     const engine = new Engine(canvas, true)
     const scene = new Scene(engine)
-    const camera: ArcRotateCamera = new ArcRotateCamera('camera', Math.PI, Math.PI, 1, Vector3.Zero())
 
+    const camera: ArcRotateCamera = new ArcRotateCamera('camera', Math.PI, Math.PI, 1, Vector3.Zero())
+    camera.attachControl(true)
 
     engine.runRenderLoop(() => {
       scene.render()
