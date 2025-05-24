28a0eef0801d036e183c57dc49dbe6372c43fa6c
Nimira43 <n.rabone72@aol.com>
2025-05-24 19:55:28 +0100

    Setting up constructor and two private methods - empty for the moment.

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,17 @@ class App {
   private _canvas: HTMLCanvasElement
   private _engine: Engine
   private _scene: Scene
-  
+
   constructor() {
-    
+    this._canvas = this._createCanvas()
+    this._initialise()
   }
+
+  private async _initialise(): Promise<> {
+    await this.main()
+  }
+  private async _createCanvas(): HTMLCanvasElement {
+    return this._canvas
+  }
+
 }
