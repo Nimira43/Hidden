257ce8c0f4cf621e1f8a8ead0b848c6f1e736102
Nimira43 <n.rabone72@aol.com>
2025-05-24 21:42:29 +0100

    Importing Game and Status into start.ts. Creating start function

--- src/states/start.ts
+++ src/states/start.ts
@@ -1,3 +1,6 @@
+import { Game, Status } from './../app';
 import { ArcRotateCamera, Engine, Scene, Vector3 } from '@babylonjs/core'
 
 import { AdvancedDynamicTexture, Image, Rectangle } from '@babylonjs/gui'
+
+export async function start() { }
