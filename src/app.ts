ad910978e32aa863919a6fafae97e6aa59b49c54
Nimira43 <n.rabone72@aol.com>
2025-05-24 19:51:16 +0100

    Creating App class and three private variables.

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,12 @@
 import { Engine, EngineFactory, Scene, ArcRotateCamera, Vector3 } from '@babylonjs/core'
 
+class App {
+
+  private _canvas: HTMLCanvasElement
+  private _engine: Engine
+  private _scene: Scene
+  
+  constructor() {
+    
+  }
+}
